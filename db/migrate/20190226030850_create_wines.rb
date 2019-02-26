c62f355e9c8e8d1221f7bd110e514d913eebba16
miirbat <mirna.plakalovic@gmail.com>
2019-02-25 22:19:12 -0500

    run migration

--- db/migrate/20190226030850_create_wines.rb
+++ db/migrate/20190226030850_create_wines.rb
@@ -1,12 +1,12 @@
-class CreateWineTable < ActiveRecord::Migration[5.2]
+class CreateWines < ActiveRecord::Migration[5.2]
   def change
     create_table :wines do |t|
       t.string :name
       t.text :description
       t.string :country
       t.string :vintage
-      t.string :varietal
-      :t.string
+      t.string :grape
+      t.string :type
 
       t.timestamps
     end
